bef4b051f9d6b547ed3f68a69ff8aa7cdc86e966
Ouzed <mhamed.ouzed@edu.uiz.ac.ma>
2024-02-12 21:39:10 +0100

    i added version of dictionary

--- main_diict.py
+++ main_diict.py
@@ -2,8 +2,12 @@ import json
 import os
 def Ajouter():
     try:
-        mylist1=[]
         n=int(input("Enter the number of articles: "))
+        if os.path.exists("products1.json"):
+            with open("products1.json","r") as myfile:
+                mylist=json.load(myfile)
+        else:
+            mylist={}
         while n!=0:
             mydic={}
             mydic["Reference"]=int(input("Enter the reference of the article: "))
@@ -14,27 +18,20 @@ def Ajouter():
             mydic["categorie"]=categorie
             mydic["prix"]=float(input("Enter the price of the article: "))
             mydic["stock"]=int(input("Enter the stock: "))
-            mylist1.append(mydic)
+            mylist[mydic["Reference"]]=mydic
             n-=1
-        if os.path.exists("products.json"):
-            with open("products.json","r") as myfile:
-                mylist=json.load(myfile)
-        else:
-            mylist=[]
-        for el in mylist1:
-            mylist.append(el)
-        with open("products.json","w") as myfile:
+        with open("products1.json","w") as myfile:
             json.dump(mylist,myfile,indent=2)
     except Exception as e:
         print(f"Error: {e}")
 
 def Chercher(reference):
     try:
-        with open("products.json","r") as myfile:
+        with open("products1.json","r") as myfile:
             mylist=json.load(myfile)
             for el in mylist:
-                if reference==el["Reference"]:
-                     print(el)
+                if reference==int(el):
+                     print(mylist[el])
                      return
             print("ne trouve pas article avec cette reference")
     except Exception as e:
@@ -43,11 +40,11 @@ def List_par_catego(categorie):
     mycatego=[]
     if categorie.lower() in ["mobilier","nettoyage","patriciere","electronic"]:
         try:
-            with open("products.json","r") as myfile:
+            with open("products1.json","r") as myfile:
                 mylist=json.load(myfile)
                 for el in mylist:
-                    if el["categorie"].lower()== categorie.lower():
-                        mycatego.append(el)
+                    if mylist[el]["categorie"].lower()== categorie.lower():
+                        mycatego.append(mylist[el])
                 if not mycatego:
                     print("pas des article de cette categorie!!")
                     return
@@ -59,7 +56,7 @@ def List_par_catego(categorie):
         print("entrer correct Categorie!!!!")
 def Afficher():
     try:
-        with open("products.json","r") as myfile:
+        with open("products1.json","r") as myfile:
             mylist=json.load(myfile)
             print(mylist)
     except:
@@ -67,7 +64,7 @@ def Afficher():
 
 def Inventairephysique():
     try:
-        with open("products.json","r") as myfile:
+        with open("products1.json","r") as myfile:
             mylist=json.load(myfile)
             total_des_articles=0
             Stock_mobilier=0
@@ -75,18 +72,18 @@ def Inventairephysique():
             Stock_nettoyage=0
             Stock_electronic=0
             for el in mylist:
-                if el["categorie"].lower()=="mobilier":
-                  total_des_articles+=el["stock"]
-                  Stock_mobilier+=el["stock"]
-                if el["categorie"].lower()=="nettoyage":
-                  total_des_articles+=el["stock"]
-                  Stock_nettoyage+=el["stock"]
-                if el["categorie"].lower()=="patriciere":
-                  total_des_articles+=el["stock"]
-                  Stock_patricier+=el["stock"]
-                if el["categorie"].lower()=="electronic":
-                  total_des_articles+=el["stock"]
-                  Stock_electronic+=el["stock"]
+                if mylist[el]["categorie"].lower()=="mobilier":
+                  total_des_articles+=mylist[el]["stock"]
+                  Stock_mobilier+=mylist[el]["stock"]
+                if mylist[el]["categorie"].lower()=="nettoyage":
+                  total_des_articles+=mylist[el]["stock"]
+                  Stock_nettoyage+=mylist[el]["stock"]
+                if mylist[el]["categorie"].lower()=="patriciere":
+                  total_des_articles+=mylist[el]["stock"]
+                  Stock_patricier+=mylist[el]["stock"]
+                if mylist[el]["categorie"].lower()=="electronic":
+                  total_des_articles+=mylist[el]["stock"]
+                  Stock_electronic+=mylist[el]["stock"]
             print("###################INVENTAIREPHYSIQUE###############################")
             print(f"Stock_mobilier : {Stock_mobilier} Article")
             print(f"Stock_patricier : {Stock_patricier} Article")
@@ -98,7 +95,7 @@ def Inventairephysique():
 
 def InventaireComptable():
     try:
-        with open("products.json","r") as myfile:
+        with open("products1.json","r") as myfile:
             mylist=json.load(myfile)
             Prixtotal_des_articles=0
             Prix_des_mobilier=0
@@ -106,18 +103,18 @@ def InventaireComptable():
             Prix_des_nettoyage=0
             Prix_des_electronic=0
             for el in mylist:
-                if el["categorie"].lower()=="mobilier":
-                  Prixtotal_des_articles+= (el["stock"]*el["prix"])
-                  Prix_des_mobilier+= (el["stock"]*el["prix"])
-                if el["categorie"].lower()=="patriciere":
-                  Prixtotal_des_articles+= (el["stock"]*el["prix"])
-                  Prix_des_patricier+= (el["stock"]*el["prix"])
-                if el["categorie"].lower()=="electronic":
-                  Prixtotal_des_articles+= (el["stock"]*el["prix"])
-                  Prix_des_electronic+= (el["stock"]*el["prix"])
-                if el["categorie"].lower()=="nettoyage":
-                  Prixtotal_des_articles+= (el["stock"]*el["prix"])
-                  Prix_des_nettoyage+= (el["stock"]*el["prix"])
+                if mylist[el]["categorie"].lower()=="mobilier":
+                    Prixtotal_des_articles+=(mylist[el]["stock"]*mylist[el]["prix"])
+                    Prix_des_mobilier+=(mylist[el]["stock"]*mylist[el]["prix"])
+                if mylist[el]["categorie"].lower()=="patriciere":
+                    Prixtotal_des_articles+=(mylist[el]["stock"]*mylist[el]["prix"])
+                    Prix_des_patricier+=(mylist[el]["stock"]*mylist[el]["prix"])
+                if mylist[el]["categorie"].lower()=="electronic":
+                    Prixtotal_des_articles+=(mylist[el]["stock"]*mylist[el]["prix"])
+                    Prix_des_electronic+=(mylist[el]["stock"]*mylist[el]["prix"])
+                if mylist[el]["categorie"].lower()=="nettoyage":
+                    Prixtotal_des_articles+=(mylist[el]["stock"]*mylist[el]["prix"])
+                    Prix_des_nettoyage+=(mylist[el]["stock"]*mylist[el]["prix"])
 
             print("###################INVENTAIREComptable###############################")
             print(f"Prix_des_mobilier : {Prix_des_mobilier} DH")
@@ -129,22 +126,24 @@ def InventaireComptable():
         print("pas des articles!!!!")
 def Modifier(reference):
     try:
-        with open("products.json","r") as myfile:
+        with open("products1.json","r") as myfile:
             mylist=json.load(myfile)
         found=False
         for el in mylist:
-            if el["Reference"]==reference:
+            if mylist[el]["Reference"]==reference:
                 found=True
                 new_ref=""
                 while type(new_ref)!=int:
                     new_ref=int(input("entrer new Reference : "))
-                el["Reference"]=new_ref
+                mylist[new_ref]=mylist.pop(el)
+                mylist[new_ref]["Reference"]=new_ref
         if found:
-            with open("products.json","w") as myfile:
+            with open("products1.json","w") as myfile:
                 json.dump(mylist,myfile,indent=2)
         else:
             print("pas article avec votre reference")
     except:
         print("pas drs articles!!")
+
 
 
